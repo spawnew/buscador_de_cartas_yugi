306b9a334d789a5a8ed92bca68fa8a0334face01
Lucas Sorzio <sorziolucas4@gmail.com>
2025-04-24 19:16:00 -0300

    agregado de un ternario y un poco de estilos

--- src/Componentes/Vistayugi.jsx
+++ src/Componentes/Vistayugi.jsx
@@ -3,8 +3,9 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { helpHttp } from '../../helpers/helpHttp'
 import FormYugi from './FormYugi'
+import Yugi from './Yugi'
 const Vistayugi = () => {
-   const [dato, setDato] = useState({})
+   const [dato, setDato] = useState({ carta: null })
     const [yugi1, setyugi] = useState([])
  
  
@@ -15,8 +16,6 @@ const Vistayugi = () => {
                   console.log(dato)
         const fetchData = async () => {
 
-           
-           
             const yugiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
             const api = helpHttp()
            
@@ -36,15 +35,24 @@ const Vistayugi = () => {
     return (
         <div>
             <FormYugi obtener={obtener} />
-
-            {     yugi1.filter((el) => el.name === dato.carta).map((el) => (
-                <div key={el.id}>
-                    <h2>{el.name}</h2>
-                    <img src={el.card_images[0].image_url} alt={el.name} />
-                   
+            <div className="contenedor-cartas">
+            {(dato.carta===null) ? 
+               ( yugi1.map((el) => (
+                    <div className='carta' key={el.id}>
+                       <Yugi nombre={el.name}
+                           img={el.card_images[0].image_url } /> 
+                       
+                    </div>
+                )))
+            :(yugi1.filter((el) => el.name === dato.carta).map((el) => (
+                <div className='carta' key={el.id}>
+                    <Yugi nombre={el.name}
+                           img={el.card_images[0].image_url } /> 
                 </div>
-            ))}
-    </div>
+            )))}
+    
+            </div>
+            </div>
   )
 }
 
