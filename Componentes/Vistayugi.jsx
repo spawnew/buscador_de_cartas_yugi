4164c383a2539aac28b0d8ec9e772e8c75b2d897
Lucas Sorzio <sorziolucas4@gmail.com>
2025-04-24 18:21:52 -0300

    agregando la api

--- Componentes/Vistayugi.jsx
+++ Componentes/Vistayugi.jsx
@@ -5,7 +5,27 @@ const Vistayugi = () => {
  
     const [yugi1, setyugi] = useState([])
  
- 
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            if (!dato.artista)  return
+            const { artista, cancion } = dato
+            const yugiUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+            const api = helpHttp()
+           
+            const [resbusqueda, resyugi] = await Promise.all([
+                api.get(yugiUrl)
+
+            ])
+           
+            console.log(resyugi.data)
+            setyugi(resyugi.data)
+        }
+        fetchData()
+
+    }, [dato])
+
  
     return (
     <div></div>
