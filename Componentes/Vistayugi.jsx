bd6989366d6567e7918e64ed3bdf08fc59271375
Lucas Sorzio <sorziolucas4@gmail.com>
2025-04-24 18:19:36 -0300

    primer commit

---
+++ Componentes/Vistayugi.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+const Vistayugi = () => {
+ 
+    const [yugi1, setyugi] = useState([])
+ 
+ 
+ 
+    return (
+    <div></div>
+  )
+}
+
+export default Vistayugi
