af48ffef78949768a3fab493b339fe53ddfa9fef
Lucas Sorzio <sorziolucas4@gmail.com>
2025-12-23 11:48:28 -0300

    sa

--- src/Componentes/Contacto/Redes.jsx
+++ src/Componentes/Contacto/Redes.jsx
@@ -6,7 +6,7 @@ const Redes = ({ nombre, link, imagen }) => {
     return (
         <div className='flex md:flex-col flex-row justify-center w-20 md:w-50   items-center md:m-4 hover:scale-105 transition-transform duration-300'>
                 <a href={link} >
-                <img className='rounded-lg w-8 h-8' src={imagen} alt={nombre} />
+                <img className='rounded-lg w-5 h-5' src={imagen} alt={nombre} />
                 <p className='text-amber-100 text-center'>{nombre}</p>
             </a>
         </div>
